641b845a9feb32491c951a428c2456d503ec0b70
Avantika Adiyodi Veenus <33176830+avantikaadiyodi@users.noreply.github.com>
2025-02-18 20:19:49 +0530

    update pipo_register.v to 4 bit

--- pipo_register.v
+++ pipo_register.v
@@ -1,11 +1,11 @@
 module pipo_register (clk, reset, load, data_in, data_out);
     input wire clk, reset, load;
-    input reg [7:0] data_in;
-    output reg [7:0] data_out;
+    input reg [3:0] data_in;
+    output reg [3:0] data_out;
 
     always@(posedge clk or posedge reset) begin
         if (reset) begin
-            data_out<=8'b00000000;
+            data_out<=4'b0000;
         end
         else if (load) begin
             data_out <= data_in;
